ebb3e00c7a1102688b933e56be4669b57ec95413
Zack <32884367+zackeaton@users.noreply.github.com>
2021-05-20 14:26:37 -0400

    Addded new page

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,13 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":630},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Gatsby Starter Blog","short_name":"GatsbyJS","start_url":"/","background_color":"#ffffff","theme_color":"#663399","display":"minimal-ui","icon":"content/assets/gatsby-icon.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"4a9773549091c227cd2eb82ccd9c5e3a"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ content/blog/from-the-crevasse/from-the-crevasse.md
@@ -0,0 +1,9 @@
+---
+title: From The Crevasse 🏔 
+date: "2021-05-20T23:46:37.121Z"
+description: An Exploratory Journey.
+---
+
+I am crawling out from the crevasse. Both mentally and physically. Yesterday I spent some much needed time updating my personal laptop. After months of neglect from keeping things up-to-date, everything hit at once. My personal dev site is unable to be updated due to a couple out of date files. I should probably redo everything again and start from scratch. 
+
+*ugh*
